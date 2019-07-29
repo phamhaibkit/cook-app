02c6cec86931ef83f9c38cabea166958b5a0b4a4
Ngoc SOn <sontn94@gmail.com>
2019-07-29 20:32:32 +0700

    Recipe highlight list, love list

--- CookApp/src/components/recipe-highlight-list/recipe-highlight-list.js
+++ CookApp/src/components/recipe-highlight-list/recipe-highlight-list.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import RecipeHighlightHome from '../recipe-highlight-home/recipe-highlight-home';
 import { COLOR } from '../../utils/variables';
+import recipeService from '../../services/recipe.service';
+
 
 export default class RecipeHighlightList extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      recipes: []
+    }
+    this.recipes;
+  }
+
+  componentWillMount() {
+    recipeService.getRecipeHighLightList().then(() => {
+      this.setState({
+        recipes : recipeService.recipeHighLightData.recipes
+      })
+    })
   }
   render() {
-    const { navigation } = this.props;
-    const isHightLight = navigation && navigation.state.params;
+    const { recipes } = this.state;
     return (
       <View style={{ flex: 1, paddingHorizontal: 15, backgroundColor: COLOR.backgroundColor }}>
-        <RecipeHighlightHome isHightLight={isHightLight}/>
+        <RecipeHighlightHome recipes={recipes}/>
       </View>
     );
   }
