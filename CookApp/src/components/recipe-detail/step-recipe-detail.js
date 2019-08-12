6faba76c8d400206026f04e2d61b5a35943171d6
Son Tran <tnson@tma.com.vn>
2019-08-12 19:56:12 +0700

    update code detail recipe

--- CookApp/src/components/recipe-detail/step-recipe-detail.js
+++ CookApp/src/components/recipe-detail/step-recipe-detail.js
@@ -1,53 +1,57 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { CSS, IMG } from '../../utils/variables';
 
 export default class StepRecipeDetail extends Component {
   static propTypes = {
     // prop: PropTypes
   }
+
   constructor(props) {
     super(props);
     this.state = {
-      activeImage: 0
-    }
+      activeImage: 0,
+    };
   }
 
   render() {
-    const { data } = this.props
+    const { data } = this.props;
     const { stepImages, stepDetail } = data.infor;
     const { activeImage } = this.state;
     return <View style={[styles.stepsSection, !data.lastChild ? CSS.borderBottom : '']}>
       <View style={[CSS.flexRow, CSS.alignItemsCenter, { marginBottom: 7 }]}>
-        <Image style={{ height: 6, width: 6, marginRight: 5 }} source={IMG.greenCircle}></Image>
+        <Image style={{ height: 6, width: 6, marginRight: 5 }} source={IMG.greenCircle} />
         <Text style={[CSS.fontSize14, CSS.fontQuiBold, styles.colorTextDark]}>Bước {data.stepNumber}</Text>
       </View>
       {stepImages && stepImages.length !== 0 && <View style={[styles.stepImages]}>
-        <Image style={[{ height: 214, width: '100%', borderRadius: 5, marginBottom: 5 }]} source={{ uri: stepImages[activeImage] }}></Image>
+        <Image style={[{ height: 214, width: '100%', borderRadius: 5, marginBottom: 5 }]} source={{ uri: stepImages[activeImage] }} />
         <View style={[CSS.flexRow, CSS.alignItemsCenter, CSS.justifyContentCenter]}>
           {
             stepImages.map((itemImage, index) => {
-              return <TouchableOpacity style={{ paddingHorizontal: 1 }} onPress={() => {
-                this.setState({
-                  activeImage: index
-                })
-              }} key={index}><View>
+              return <TouchableOpacity
+                style={{ paddingHorizontal: 1 }}
+                onPress={() => {
+                  this.setState({
+                    activeImage: index
+                  });
+                }}
+                key={index}><View>
                   <Image source={{ uri: itemImage }} style={{ height: 66, width: 85, borderRadius: 5, tintColor: 'white' }} />
                   <Image source={{ uri: itemImage }} style={{ height: 66, width: 85, borderRadius: 5, position: 'absolute', opacity: activeImage === index ? 1 : 0.3 }} />
                 </View>
-              </TouchableOpacity>
+              </TouchableOpacity>;
             })}
         </View>
       </View>}
-      
+
       <View style={[{ marginTop: 7 }]}>
         <Text style={[CSS.fontSize14, CSS.fontQuiRegular, { lineHeight: 24 }]}>
           {stepDetail}
-          </Text>
+        </Text>
       </View>
-    </View>
+    </View>;
   }
 }
 
@@ -57,4 +61,4 @@ const styles = StyleSheet.create({
     marginTop: 22,
     paddingBottom: 15,
   },
-})
+});
