3b306f3ee7e786754e2d7385efda4df5ce79d421
nong <nongtranngoc@gmail.com>
2019-08-14 00:29:20 +0700

    add scrollable-tab-view module + add handle save button collection item

---
+++ CookApp/src/components/user-reviewing-recipe/user-reviewing-recipe.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class UserReviewingRecipe extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text> prop </Text>
+      </View>
+    )
+  }
+}
+
+
